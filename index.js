be80e73f8f6f5f5d57e9873f7ab12dd77cbc38e0
MechaLarry <mecha.larry@gmail.com>
2024-07-12 21:22:43 +0200

    add files

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  // createFareMultiplier()
+  const createFareMultiplier = function (multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  };
+
+  // fareDoubler()
+  const fareDoubler = createFareMultiplier(2);
+
+  // fareTripler()
+  const fareTripler = createFareMultiplier(3);
+
+  //selectDifferentDrivers(arrayOfDrivers, function)
+  const selectDifferentDrivers = function (drivers, func) {
+    return func(drivers);
+  };
